f2ac02ed6dedac499ec93f5c9e96a694c3a19618
big-good-boy <drago999@bk.ru>
2018-02-14 23:19:05 +0500

    Изменение

--- pizza.py
+++ pizza.py
@@ -13,9 +13,15 @@
 # for topping in pizza['toppings']:
 #     print("\t" + topping)
 
+# def make_pizza(*toppings):
+#     """Вывод списка заказанных дополнений."""
+#     print(toppings)
+
 def make_pizza(*toppings):
     """Вывод списка заказанных дополнений."""
-    print(toppings)
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
 
 make_pizza('popperoni')
 make_pizza('mushrooms', 'green peppers', 'extra cheese')
