c19f8be447724d830e41a2bef128b7bdc879f0c0
Andrej <drago999@bk.ru>
2018-02-27 17:57:21 +0500

    Изменения

--- 9-14.py
+++ 9-14.py
@@ -5,8 +5,22 @@ class Die():
     self.sides = sides
 
   def roll_die(self):
-    return randint(1, self.sides)
+    return(randint(1, self.sides))
 
-cub = Die();
+cub6 = Die()
+x = 1
+while x < 10:
+  print(cub6.roll_die())
+  x += 1
 
-print(cub)
+cub6 = Die(10)
+x = 1
+while x < 10:
+  print(cub6.roll_die())
+  x += 1
+
+cub6 = Die(20)
+x = 1
+while x < 10:
+  print(cub6.roll_die())
+  x += 1
