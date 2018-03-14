7ccd92bb3226dac61d1fd5017eae0d971a7c2a47
Andrej <drago999@bk.ru>
2018-03-14 18:06:04 +0500

    Изменения

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -12,13 +13,19 @@ def run_game():
     (ai_settings.screen_width, ai_settings.screen_height))
   pygame.display.set_caption("Alien Invasion")
 
-  # Создание корабля.
+  # Создание корабля, группы пуль и группы пришельцев
   ship = Ship(ai_settings, screen)
+  bullets = Group()
+  aliens = Group()
+
+  # Создание флота пришельцев.
+  gf.create_fleet(ai_settings, screen, ship, aliens)
 
   # Запуск основного цикла игры.
   while True:
-    gf.check_events(ship)
+    gf.check_events(ai_settings, screen, ship, bullets)
     ship.update()
-    gf.update_screen(ai_settings, screen, ship)
+    gf.update_bullets(bullets)
+    gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
