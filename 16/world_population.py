6be5bc1d4c096ad6837182a70b4997c2311da3f0
Andrej <drago999@bk.ru>
2018-03-23 17:16:39 +0500

    Изменения

---
+++ 16/world_population.py
@@ -0,0 +1,45 @@
+import json
+
+from pygal.maps.world import World
+from pygal.style import LightColorizedStyle, RotateStyle
+
+from country_codes import get_country_code
+
+# Список заполняется данными.
+filename = 'population_data.json'
+with open(filename) as f:
+  pop_data = json.load(f)
+
+# Вывод населения каждой страны за 2010 год.
+# Построение словаря с данными численности населения.
+cc_populations = {}
+for pop_dict in pop_data:
+  if pop_dict['Year'] == '2010':
+    country = pop_dict['Country Name']
+    population = int(float(pop_dict['Value']))
+    code = get_country_code(country)
+    if code:
+      cc_populations[code] = population
+
+# Группировка стран по 3 уровням населения.
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+  if pop < 10000000:
+    cc_pops_1[cc] = pop
+  elif pop < 1000000000:
+    cc_pops_2[cc] = pop
+  else:
+    cc_pops_3[cc] = pop
+
+# Проверка количества стран на каждом уровне.
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+wm = World()
+wm_style = RotateStyle('#336699', base_style=LightColorizedStyle)
+wm = World(style=wm_style)
+wm.title = 'World Populantion in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+
+wm.render_to_file('world_population.svg')
